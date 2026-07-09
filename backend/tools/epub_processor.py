8fa6a755366ba4d4b3503386edfa6a033ad23da7
mofumahou <91860507+mofumahou@users.noreply.github.com>
2026-07-09 03:20:31 -0400

    [FEAT] added method to extract html from epub and store it as a list of dictionaries containing a chapter id, name, and bs4 soup object

--- backend/tools/epub_processor.py
+++ backend/tools/epub_processor.py
@@ -2,40 +2,63 @@ import ebooklib
 from pathlib import Path
 from ebooklib import epub
 from bs4 import BeautifulSoup
-from markdownify import markdownify as md
+### from markdownify import markdownify as md
 
 from app.config import CORPUS_DIR, EPUB_DIR
 
-# This script processes EPUB files by extracting their html content
-# Preliminary step to investigate html structure before stripping html tags and converting to markdown
+# This script processes EPUB files by extracting their html and converting it to markdown.
 
-def process_epub(epub_path, output_dir):
-    output_dir.mkdir(parents=True, exist_ok=True)
+# Processes an EPUB file extracting only the core text and document data, saving 
+#   it as a list of dictionaries, each representing a chapter in the book.
+def extract_html_chapters(epub_path: Path) -> list[dict]:
     book = epub.read_epub(str(epub_path))
-    document_ids_to_skip = ["pg-header", "cover", "item4", "item5", "ENDNOTE", "pg-footer", "ncx", "coverpage-wrapper"]
     
-    documents = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-    for item in documents:
-        if item.get_id() in document_ids_to_skip:
-            continue
-        content = item.get_content()
-        output_file_path = output_dir / f"{item.get_name()}.html"
-        output_file_path.write_bytes(content)
-        #print(f"Item ID: {item.get_id()} | Name: {item.get_name()}")
+    # Strips out front matter and back matter, keeps only the main content of the book
+    document_ids_to_skip = {"pg-header", 
+                            "cover", 
+                            "item4", 
+                            "item5", 
+                            "ENDNOTE", 
+                            "pg-footer", 
+                            "ncx", 
+                            "coverpage-wrapper"}
+    
+    document = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+    chapters = []
 
-def convert_html_to_markdown(html_content):
-    soup = BeautifulSoup(html_content, "html.parser").prettify()
+    for item in document:
+        if item.get_id() not in document_ids_to_skip:
+            soup = BeautifulSoup(item.get_content(), "html.parser")
+            chapters.append(create_chapter_dictionary(item.get_id(), item.get_name(), soup))
+    return chapters
 
 
+# Creates a dictionary for each chapter in the EPUB file, preserving the chapter id, name, and content (as a BeautifulSoup object)
+def create_chapter_dictionary(chapter_id: str, chapter_name: str, soup: BeautifulSoup) -> dict:
+    chapter_dict = {
+        "id": chapter_id,
+        "name": chapter_name,
+        "content": soup
+        }
+    print(f" Extracting:> Chapter ID: {chapter_dict['id']} | Name: {chapter_dict['name']} | Length: {len(chapter_dict['content'].get_text())} characters")  # Prints the chapter id and name to the console for tracking progress
+    return chapter_dict
 
-def main():
+
+# <--------- TO WORK ON NEXT --------->
+def clean_chapters(chapters: list[dict]) -> str:
+    # Strip out unwanted HTML tags, 
+    pass
+
+
+def main() -> None:
     epub_path = next(EPUB_DIR.glob("*.epub"), None)
     if epub_path is None:
         raise FileNotFoundError(f"No EPUB files found in {EPUB_DIR}")
         
     output_dir = CORPUS_DIR / "html" / epub_path.stem
+    chapters = extract_html_chapters(epub_path)
+    # chapters will be used to create a markdown version of the book in the next step of the pipeline
 
-    process_epub(epub_path, output_dir)
 
 if __name__ == "__main__":
     main()
