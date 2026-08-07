d83368b900252def9b0f0b0a669aeb3bd924f741
mofumahou <91860507+mofumahou@users.noreply.github.com>
2026-08-07 17:02:51 -0400

    [REFACTOR] Project is properly commented and final formatting fixes are applied.

--- backend/tools/epub_processor.py
+++ backend/tools/epub_processor.py
@@ -2,25 +2,24 @@ import re
 import ebooklib
 from pathlib import Path
 from ebooklib import epub
-from bs4 import BeautifulSoup, NavigableString, Comment, Tag, Doctype
-from markdownify import markdownify as md
+from bs4 import BeautifulSoup, NavigableString, Comment, Tag
+from markdownify import markdownify as md # TO BE DELETED
 
 from app.config import CORPUS_DIR, EPUB_DIR
 
-# This script processes EPUB files by extracting their html and converting it to markdown.
-
-# Processes an EPUB file extracting only the core text and document data, saving 
-#   it as a list of dictionaries, each representing a chapter in the book.
+# Processes an EPUB file extracting only the html and document data, saving 
+# it as a list of dictionaries, each representing a chapter in the book.
 
 PAGE_SPAN_CLASS = "x-ebookmaker-pageno"
 SECTION_RE = re.compile(r"^(\d+)\.\s+(.+)")
 SUBSECTION_RE = re.compile(r"^\s*\((\d+)\)\s*$")
 
+# Pull the html content of each chapter in the EPUB file, returning a list of dictionaries
 def extract_html_chapters(epub_path: Path
                           ) -> list[dict]:
     book = epub.read_epub(str(epub_path))
     
-# Strips out front matter and back matter, keeps only the main content of the book
+    # Strips out front matter and back matter, keeps only the main content of the book
     document_ids_to_skip = {"pg-header", 
                             "cover", 
                             "item4", 
@@ -41,7 +40,6 @@ def extract_html_chapters(epub_path: Path
             chapters.append(create_chapter_dictionary(item.get_id(), item.get_name(), soup))
     return chapters
 
-
 # Creates a dictionary for each chapter in the EPUB file, preserving the chapter id, name, and content (as a BeautifulSoup object)
 def create_chapter_dictionary(chapter_id: str, 
                               chapter_name: str, 
@@ -55,18 +53,17 @@ def create_chapter_dictionary(chapter_id: str,
     print(f" Extracting:> Chapter ID: {chapter_dict['id']} | Name: {chapter_dict['name']} | Length: {len(chapter_dict['soup'].get_text())} characters")  # Prints the chapter id and name to the console for tracking progress
     return chapter_dict
 
-# <--------- HELPER FUNCTIONS ---------------->
-
+# Main formatter function
 def clean_chapters(chapters: list[dict]
                    ) -> list[dict]:
-    # Strip out unwanted HTML tags,
+    # Main function to clean the html content of each chapter
     for chapter in chapters:
         soup = chapter["soup"]
 
         # Convert page spans to soup comments
         clean_chapters_pageno_helper(soup)
 
-        # Remove noise
+        # Remove unwanted tags and attributes
         for tag in soup.find_all(["script", "style", "hr", "table"]):
             tag.decompose()
         for footnotes in soup.find_all("div", class_="footnotes"):
@@ -81,20 +78,24 @@ def clean_chapters(chapters: list[dict]
         for header in soup.find_all(["h4", "h5"]):
             header.unwrap()
 
+        # Call helper functions to format the chapters into a clean structure with proper header tags
+        clean_parts_helper(soup)
         clean_articles_helper(soup)
         clean_sections_helper(soup)
         clean_subsections_helper(soup)
 
+        # Remove last bit of noise
         for tag in soup.find_all(["span"]):
             tag.unwrap()
 
-        for tag in soup.find_all(True):  # every tag
+        # Match all tags and remove all attributes
+        for tag in soup.find_all(True):
             tag.attrs = {}
-        
+                    
         chapter["soup"] = soup
     return chapters
 
-
+# <--------- HELPER FUNCTIONS ---------------->
 def clean_chapters_pageno_helper(soup: BeautifulSoup
                                  ) -> None:
     for span in soup.find_all("span", class_=PAGE_SPAN_CLASS):
@@ -105,10 +106,30 @@ def clean_chapters_pageno_helper(soup: BeautifulSoup
             span.replace_with(NavigableString("\n\n"), comment, NavigableString("\n\n"))
         else:
             span.decompose()
+    return
+
+
+def clean_articles_helper(soup: BeautifulSoup
+                          ) -> None:
+    for article in list(soup.find_all("h3")):
+        article.string = article.get_text(strip=True).removesuffix(".")
+    return
+
+
+def clean_parts_helper(soup: BeautifulSoup
+                       ) -> None:
+    # extract title and replace h2 text with it
+    for part in list(soup.find_all("h2")):
+        h2_title = part.attrs.get("title")
+        if h2_title is not None:
+            part.string = h2_title.removesuffix(".")
+        else:
+            continue
+    return
 
 
 def clean_sections_helper(soup: BeautifulSoup
-                                 ) -> None:
+                          ) -> None:
     for span in soup.select('p > b > span[id^="sec_"]'):
         b = span.parent
         if not b:
@@ -124,13 +145,14 @@ def clean_sections_helper(soup: BeautifulSoup
 
         section_num, section_title = match.groups()
         h4 = soup.new_tag("h4")
-        h4.string = f"{section_num}. {section_title}"
+        h4.string = f"{section_num}. {section_title}".removesuffix(".")
 
         p.insert_before(h4)
 
         b.decompose()
 
         if not p.get_text(strip=True): p.decompose()
+    return
 
 
 def clean_subsections_helper(soup: BeautifulSoup
@@ -150,7 +172,7 @@ def clean_subsections_helper(soup: BeautifulSoup
         subsection_title = " ".join(second_child.get_text().split())
 
         h5 = soup.new_tag("h5")
-        h5.string = f"({subsection_num}) {subsection_title}"
+        h5.string = f"({subsection_num}) {subsection_title}".removesuffix(".")
 
         p.insert_before(h5)
 
@@ -158,36 +180,28 @@ def clean_subsections_helper(soup: BeautifulSoup
         second_child.decompose()
 
         if not p.get_text(strip=True): p.decompose()
-
-
-def clean_articles_helper(soup: BeautifulSoup
-                             ) -> None:
-    # extract title and replace h2 text with it
-    for article in list(soup.find_all("h2")): 
-        h2_title = article.attrs.get("title")
-        if h2_title is not None:
-            article.string = h2_title
-        else:
-            continue
     return
 
-
-# <--------- SAVE PROCESSED CHAPTERS --------->
-def save_chapters_as_markdown(chapters: list[dict], 
-                              output_dir: Path
-                              ) -> None:
+# <--------- (TO BE DELETED) SAVE PROCESSED CHAPTERS --------->
+# Purely for debugging purposes
+def save_chapters(chapters: list[dict],
+                  output_dir: Path
+                  ) -> None:
     ### book = []
     output_dir.mkdir(parents=True, exist_ok=True)
 
     for chapter in chapters:
-        output_file = output_dir / f"{chapter['name']}.html"            #!!!!!!!!!!!!file extension MUST be changed to .md in finalized version of the pipeline
+        output_file = output_dir / f"{chapter['name']}.html"
         with open(output_file, "w", encoding="utf-8") as f:
             f.write(str(chapter["soup"]))
         markdown_output_file = output_dir / f"{chapter['name']}.md"
         with open(markdown_output_file, "w", encoding="utf-8") as f:
             f.write(md(str(chapter["soup"]), heading_style="ATX"))
+    return
 
-
+# <--------- (TO BE MODIFIED) MAIN FUNCTION --------->
+# Needs changing to integrate with ingestion_pipeline.py, no files will be generated in final version
+# Instead the processed chapters will be returned to ingestion_pipeline.py for chunking, embedding, and storage.
 def main() -> None:
     epub_path = next(EPUB_DIR.glob("*.epub"), None)
     if epub_path is None:
@@ -199,7 +213,7 @@ def main() -> None:
     print(f"Extracted {len(unprocessed_chapters)} chapters from {epub_path.name}")
     processed_chapters = clean_chapters(unprocessed_chapters)
     print(f"Processed {len(processed_chapters)} chapters from {epub_path.name}")
-    save_chapters_as_markdown(processed_chapters, output_dir)
+    save_chapters(processed_chapters, output_dir)
 
 
 if __name__ == "__main__":
